2717375cf147a727617bbb619e643ec399b4f77e
Denis Michailov <denis.michailov@gmail.com>
2016-11-16 17:56:55 +1000

    fix server for local

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 const port = process.env.PORT || 3333;
 
 app.use(function(req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else {
+	if(req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 app.use(express.static('public'));
